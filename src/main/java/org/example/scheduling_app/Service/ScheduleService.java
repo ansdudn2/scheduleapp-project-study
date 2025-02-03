bc3b8670334cd5d8d072cf0f84f6c7b0b00b71c5
ansdudn3@gmail.com <ansdudn3@gmail.com>
2025-02-03 21:09:33 +0900

    Modify schedule

--- src/main/java/org/example/scheduling_app/Service/ScheduleService.java
+++ src/main/java/org/example/scheduling_app/Service/ScheduleService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,11 +19,11 @@ public class ScheduleService {
     String formattedDate = LocalDateTime.now().format(formatter);
     private final ScheduleRepository scheduleRepository;
 
-    public ScheduleService( ScheduleRepository scheduleRepository) {
+    public ScheduleService(ScheduleRepository scheduleRepository) {
         this.scheduleRepository = scheduleRepository;
     }
 
-    public ScheduleResponseDto createSchedule(ScheduleRequestDto dto){
+    public ScheduleResponseDto createSchedule(ScheduleRequestDto dto) {
         //Schedule 엔티티 객체 생성
         Schedule schedule = new Schedule(dto.getTask(), dto.getAuthor(), dto.getPassword(), LocalDateTime.parse(formattedDate, formatter));
         //데이터베이스에 저장하고 응답을 반환
@@ -43,5 +44,23 @@ public class ScheduleService {
         // 선택한 일정 조회
         Schedule schedule = scheduleRepository.findScheduleById(id);
         return new ScheduleResponseDto(schedule);
+    }
+
+    // 일정 수정
+    public ScheduleResponseDto updateSchedule(Long id, ScheduleRequestDto dto) {
+        // 기존 일정 조회
+        Schedule existingSchedule = scheduleRepository.findScheduleById(id);
+
+        // 비밀번호 검증
+        if (!existingSchedule.getPassword().equals(dto.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+        // 수정할 항목 갱신
+        existingSchedule.update(dto.getTask(), dto.getPassword()); // 할일과 비밀번호 수정
+        existingSchedule.setAuthor(dto.getAuthor()); // 작성자명 수정
+        existingSchedule.setUpdatedAt(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS)); // 수정일을 현재 시간으로 갱신
+
+        // 수정된 일정 저장 후, 응답용 DTO 반환
+        return scheduleRepository.saveSchedule(existingSchedule);
     }
 }
--- src/main/java/org/example/scheduling_app/dto/ScheduleResponseDto.java
+++ src/main/java/org/example/scheduling_app/dto/ScheduleResponseDto.java
@@ -5,7 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.example.scheduling_app.entity.Schedule;
 
-
+import java.time.format.DateTimeFormatter;
 
 
 @Getter
@@ -17,11 +17,14 @@ public class ScheduleResponseDto {
     private String createdAt;
     private String updatedAt;
 
+    // DateTimeFormatter 사용하여 포맷 지정
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+
     public ScheduleResponseDto(Schedule schedule) {
         this.id = schedule.getId();
         this.task = schedule.getTask();
         this.author = schedule.getAuthor();
-        this.createdAt = schedule.getUpdatedAt().toString();
-        this.updatedAt = schedule.getUpdatedAt().toString();
+        this.createdAt = schedule.getCreatedAt().format(formatter); // createdAt 포맷
+        this.updatedAt = schedule.getUpdatedAt().format(formatter); // updatedAt 포맷
     }
 }
