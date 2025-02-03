da9117489070ca5b297f70148a1196bfd4f3ac24
ansdudn3@gmail.com <ansdudn3@gmail.com>
2025-02-03 16:16:44 +0900

    create schedule completion

--- src/main/java/org/example/scheduling_app/Service/ScheduleService.java
+++ src/main/java/org/example/scheduling_app/Service/ScheduleService.java
@@ -7,19 +7,21 @@ import org.example.scheduling_app.repository.ScheduleRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Service
 public class ScheduleService {
-
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+    String formattedDate = LocalDateTime.now().format(formatter);
     private final ScheduleRepository scheduleRepositoys;
 
-    public ScheduleService(ScheduleRepository scheduleRepository, ScheduleRepository scheduleRepositoys) {
+    public ScheduleService( ScheduleRepository scheduleRepositoys) {
         this.scheduleRepositoys = scheduleRepositoys;
     }
 
     public ScheduleResponseDto createSchedule(ScheduleRequestDto dto){
         //Schedule 엔티티 객체 생성
-        Schedule schedule = new Schedule(dto.getTask(), dto.getAuthor(), dto.getPassword(), LocalDateTime.now());
+        Schedule schedule = new Schedule(dto.getTask(), dto.getAuthor(), dto.getPassword(), LocalDateTime.parse(formattedDate, formatter));
         //데이터베이스에 저장하고 응답을 반환
         return scheduleRepositoys.saveSchedule(schedule);
     }
--- src/main/java/org/example/scheduling_app/dto/ScheduleRequestDto.java
+++ src/main/java/org/example/scheduling_app/dto/ScheduleRequestDto.java
@@ -2,6 +2,7 @@ package org.example.scheduling_app.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Getter
 @AllArgsConstructor
@@ -9,5 +10,4 @@ public class ScheduleRequestDto {
     private String task; //할일
     private String author; //작성자명
     private String password;//비밀번호
-    private String createdAT;//작성일
 }
